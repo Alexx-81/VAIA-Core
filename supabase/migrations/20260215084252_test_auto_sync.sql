4c570d14e93d9e23c25686739bfd5f9cd514dc45
Alexx-81 <alexgavrilov.vd@gmail.com>
2026-02-15 10:43:28 +0200

    chore: remove test migration

--- supabase/migrations/20260215084252_test_auto_sync.sql
+++
@@ -1,5 +0,0 @@
--- Test migration to demonstrate automatic pre-commit sync
--- This migration will be automatically pushed to Supabase when committed
-
--- Example: Add a comment to test table (no actual changes)
-COMMENT ON TABLE qualities IS 'Product quality categories - updated via auto-sync test';
